af3e38995dc13a1c383f4707fdaf739b9e816fee
7 <ruuuziiir@gmail.com>
2021-01-05 17:35:20 +0800

    first

--- drawApple/ViewController.swift
+++ drawApple/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var apple: UILabel!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var shapeSelect: UISegmentedControl!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //apple.text = "🍎"
     }
+    @IBAction func shapeSelectAction(_ sender: Any) {
+        apple.text = "🍎"
+        number.text = "1"
+        slider.value = 1
 
+    }
+    
 
+    @IBAction func appleSlider(_ sender: UISlider) {
+        
+        sender.value.round()
+        number.text = String(format: "%.0f", sender.value)
+        let num = Int(sender.value)
+
+        if shapeSelect.selectedSegmentIndex == 0 {
+            apple.text = "\(square(number: num))"
+        } else if shapeSelect.selectedSegmentIndex == 1 {
+            apple.text = "\(board(number: num))"
+
+        } else if shapeSelect.selectedSegmentIndex == 2 {
+            apple.text = "\(triangle(number: num))"
+
+        } else {
+            apple.text = "\(upsideDownTriangle(number: num))"
+
+        }
+
+    }
+    
 }
 
---
+++ drawApple/appleFunc.swift
@@ -0,0 +1,90 @@
+//
+//  appleFunc.swift
+//  drawApple
+//
+//  Created by 7 on 2021/1/5.
+//
+
+import Foundation
+
+func square(number: Int) -> String {
+    var content = "\n"
+
+    for _ in 1...number {
+        for _ in 1...number {
+            content = content + "🍎"
+        }
+        content = content + "\n"
+    }
+    return content
+}
+
+func board(number: Int) -> String {
+    var content = "\n"
+    
+    for x in 1...number {
+        for y in 1...number {
+            if (x + y) % 2 == 0 {
+                content = content + "🍎"
+            } else {
+                content = content + "🍏"
+            }
+        }
+        content = content + "\n"
+    }
+    return content
+}
+
+//func pyramid(number: Int) -> String {
+//    var content = "\n"
+//    for x in 1...number {
+//        for y in 1...number {
+//            if x + y > number {
+//                content = content + "🍎"
+//            } else {
+//                content = content + "🍏"
+//            }
+//        }
+//        content = content + "\n"
+//    }
+//    return content
+//}
+
+func triangle(number: Int) -> String {
+    var content = "\n"
+    var red = ""
+    var green = ""
+
+    for x in 1...number {
+        red = red + "🍎"
+        green = green + "🍏"
+        if x % 2 != 0 {
+            content = content + red
+        } else {
+            content = content + green
+        }
+        content = content + "\n"
+    }
+
+    return content
+}
+
+func upsideDownTriangle(number: Int) -> String {
+    var content = "\n"
+
+    for x in 0...(number - 1) {
+        for y in 1...number {
+            if x / y == 0 {
+                if x % 2 != 0 {
+                    content = content + "🍏"
+                } else {
+                    content = content + "🍎"
+                }
+            }
+        }
+        content = content + "\n"
+    }
+    return content
+}
+
+
